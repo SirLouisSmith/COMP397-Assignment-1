4cd167e84d52ccf91bd3927992851a3fbaf7bd20
SirLouisSmith <someonewholives@hotmail.com>
2015-01-26 12:05:49 -0500

    Added/Modified CSS Files and Added a few Slide to the Game

--- COMP397-Assignment-1/Scripts/lib/game.js
+++ COMP397-Assignment-1/Scripts/lib/game.js
@@ -1,1 +1,14 @@
-﻿
+﻿/// <reference path="impress.js" />
+
+//source file name: game.js
+//Author: Louis Smith
+//Last Modified by: Louis Smith
+//Date Modified: 26/10/2015
+//Program Description: this is the game script that calls the impress.js file
+//and saves weither you have certain items in your inventory
+
+//Initialize impress.js
+function main() {
+    impress().init();
+    console.log("Game Initialized");
+}

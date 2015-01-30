1c0c0a6c90b07c2cbd3174a6b5b55f52b3282fa9
SirLouisSmith <someonewholives@hotmail.com>
2015-01-29 21:07:13 -0500

    Added 2 HTML Files to Continue The Game, Added a Title Logo, and Completed The Story

--- COMP397-Assignment-1/Scripts/lib/game.js
+++ COMP397-Assignment-1/Scripts/lib/game.js
@@ -3,12 +3,12 @@
 //source file name: game.js
 //Author: Louis Smith
 //Last Modified by: Louis Smith
-//Date Modified: 26/10/2015
+//Date Modified: 29/10/2015
 //Program Description: this is the game script that calls the impress.js file
-//and saves weither you have certain items in your inventory
+//and checks weither you have collected any items
 
 //Initialize impress.js
 function main() {
     impress().init();
-    console.log("Game Initialized");
+    console.log("Game Initialized"); //To make sure that the main function runs when the page loads.
 }
